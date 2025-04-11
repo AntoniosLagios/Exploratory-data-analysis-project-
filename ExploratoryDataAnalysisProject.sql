3096cc37a9e7debd17c33d4d4739505d615d1e1f
AntoniosLagios <lagiosantonios@gmail.com>
2025-04-11 13:36:04 +0300

    Update ExploratoryDataAnalysisProject.sql

--- ExploratoryDataAnalysisProject.sql
+++ ExploratoryDataAnalysisProject.sql
@@ -20,16 +20,19 @@ FROM world_layoffs.layoffs_staging2
 WHERE  percentage_laid_off IS NOT NULL;
 
 -- Which companies had 1 which is basically 100 percent of they company laid off
+
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE  percentage_laid_off = 1;
 -- these are mostly startups it looks like who all went out of business during this time
 
 -- if we order by funcs_raised_millions we can see how big some of these companies were
+
 SELECT *
 FROM world_layoffs.layoffs_staging2
 WHERE  percentage_laid_off = 1
 ORDER BY funds_raised_millions DESC;
+
 -- BritishVolt looks like an EV company, Quibi! I recognize that company - wow raised like 2 billion dollars and went under - ouch
 
 -- Companies with the biggest single Layoff
